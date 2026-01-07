40c3219978a84fceddfcedab5c7a66747753caea
Trishansh Bhardwaj <tbhardwa@qti.qualcomm.com>
2026-01-07 16:53:24 +0530

    Copy FP32 not uint16

--- src/models/embeddings.cpp
+++ src/models/embeddings.cpp
@@ -113,7 +113,7 @@ void WindowedEmbeddings::Update(Embeddings& embeddings) {
     embeddings_ = OrtValue::CreateTensor(model_.p_device_inputs_->GetAllocator(), shape_, type_);
     std::copy_n(
         full_data,
-        window_size_ * hidden_size,
+        window_size_ * hidden_size * 2,
         embeddings_->GetTensorMutableData<uint16_t>());
 
   } else if (window_index_ < num_windows_) {
@@ -125,7 +125,7 @@ void WindowedEmbeddings::Update(Embeddings& embeddings) {
     embeddings_ = OrtValue::CreateTensor(model_.p_device_inputs_->GetAllocator(), shape_, type_);
     std::copy_n(
         full_data + window_index_ * window_size_ * hidden_size,
-        window_size_ * hidden_size,
+        window_size_ * hidden_size * 2,
         embeddings_->GetTensorMutableData<uint16_t>());
 
   } else {
@@ -134,7 +134,7 @@ void WindowedEmbeddings::Update(Embeddings& embeddings) {
     embeddings_ = OrtValue::CreateTensor(model_.p_device_inputs_->GetAllocator(), shape_, type_);
     std::copy_n(
         full_data + (sequence_length - 1) * hidden_size,
-        hidden_size,
+        hidden_size * 2,
         embeddings_->GetTensorMutableData<uint16_t>());
 
   }
